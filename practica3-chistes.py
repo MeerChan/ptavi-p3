c541e8958c21d93846be6a6e57caf32dbaa77421
Isaac Merchan Blanco <imerchan@f-l3209-pc22.aulas.etsit.urjc.es>
2019-10-07 10:57:01 +0200

    cambios chistes

--- practica3-chistes.py
+++ practica3-chistes.py
@@ -27,7 +27,6 @@ class ChistesHandler(ContentHandler):
             # De esta manera tomamos los valores de los atributos
             self.calificacion = attrs.get('calificacion', "")
         elif name == 'pregunta':
-            print("hola")
             self.inPregunta = True
         elif name == 'respuesta':
             self.inRespuesta = True
@@ -37,9 +36,11 @@ class ChistesHandler(ContentHandler):
         Método que se llama al cerrar una etiqueta
         """
         if name == 'pregunta':
+            print(self.pregunta)
             self.pregunta = ""
             self.inPregunta = False
         if name == 'respuesta':
+            print(self.respuesta)
             self.respuesta = ""
             self.inRespuesta = False
 
